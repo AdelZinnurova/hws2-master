7c86eee188815b882543d8b55e1a2679d7e72861
AdelZinnurova <ad187174@bk.ru>
2025-07-04 14:10:31 +0300

    HW7-correct8

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -37,7 +37,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        const selectedItem = (e.currentTarget.value)
+        const selectedItem = Number(e.currentTarget.value)
         onChangeOption?.(selectedItem)
     }
 
